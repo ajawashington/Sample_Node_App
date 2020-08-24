1bdf52d12c2fd32022b3acdd8a611cb62937c3ef
Aja Washington <aja.washington13@gmail.com>
2020-08-24 03:58:55 -0500

    MongoDB connected

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
-//server connection 
+const path = require('path');
+
 const express = require('express');
-//app connection
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require("./util/database")
 const app = express();
 
-
-//directory paths 
-const path = require('path');
-app.use(express.static("public"));
-
-//template engines
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-//middleware
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-
-const error = require('./controllers/error')
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use('/admin', adminData);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+//   User.findById(1)
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(error.get404)
+app.use(errorController.get404);
 
-//server port 
-app.listen(3000);
+mongoConnect((client) => {
+    console.log(client)
+    console.log('DB/Server Connected')
+    app.listen(3000);
+});
