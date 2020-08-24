7bf9232f1458b167a1f42da7debe1e0947671b1f
Aja Washington <57641436+ajawashington@users.noreply.github.com>
2020-08-24 04:01:24 -0500

    Update database.js

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    "mongodb+srv://ayejah:MON1995@cluster0.qzync.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    "mongodb+srv://ayejah:<password>@cluster0.qzync.mongodb.net/<dbname>?retryWrites=true&w=majority"
     )
   .then(client => {
     console.log('Connected');
