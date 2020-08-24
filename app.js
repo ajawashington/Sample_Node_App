0eeab9692208f5c4bb94c44d98c6639ccf23201a
Aja Washington <aja.washington13@gmail.com>
2020-08-24 01:53:44 -0500

    layouts, and partials with pug

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const fs = require("fs");
+//server connection 
+const express = require('express');
+//app connection
+const app = express();
 
-fs.writeFileSync('hello.txt', 'Hello from Node.js');
+
+//directory paths 
+const path = require('path');
+app.use(express.static("public"));
+
+//template engines
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+//middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404' , {pageTitle: 'Page Not Found'});
+})
+
+//server port 
+app.listen(3000);
---
+++ controllers/product.js
@@ -0,0 +1,8 @@
+exports.getAddProductForm = (req, res, next) => {
+    res.render('add-product', 
+    {pageTitle : 'Add Product', 
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true})
+};
